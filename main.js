442ff20d3e91a225ab012927ee57ef34626c208b
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-27 15:24:53 +0200

    add posts dinamically

--- main.js
+++ main.js
@@ -55,3 +55,56 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById("container");
+
+posts.forEach(post => {
+container.innerHTML += generatePost(post);
+})
+
+function generatePost(post) {
+    const {id, content, media, author, likes, created} = post;
+    let icon;
+    if (author.image == null) {
+        icon = `
+        <span class="profile-initials">SD</span>
+        `;
+    } else {
+        icon = `
+        <img class="profile-pic" src="${author.image}" alt="${author.name}">
+        `;
+    }
+    
+    return `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                ${icon}             
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${author.name}</div>
+                <div class="post-meta__time">${created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${content}</div>
+    <div class="post__image">
+        <img src="${media}" alt="${author.name}">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>
+    `;
+}
